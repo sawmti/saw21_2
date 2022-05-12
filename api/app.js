5827ebc01a1ca7b66ebeb5e287610209641f8839
julian <julian.ibanez.figueroa@gmail.com>
2022-05-12 17:33:21 -0400

    Se agrega lectura desde el mongo

--- api/app.js
+++ api/app.js
@@ -1,22 +1,22 @@
 const express = require('express');
 var bodyParser = require('body-parser')
 const path = require('path');
-const https = require('https');
-const querystring = require("querystring")
+// const https = require('https');
+// const querystring = require("querystring")
 const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const root = path.resolve(__dirname, '..');
 const urlMongo = ""; //ACÁ PONER URL DEL MONGO Y VER DONDE PONERLA EN LAS ENV
 var jsonParser = bodyParser.json()
 const axios = require('axios');
-const { Console } = require('console');
+// const { Console } = require('console');
 // Log invocations
 app.use(function (req, res, next) { console.log(req.url); next(); });
 
 // Directly serve static content from /client
 app.use(express.static(root + '/client'));
 
-// // Simple REST API that returns some entities
+//Simple REST API that returns some entities
 // app.get('/api/entities', (req, res) =>
 //   res.send({
 //     entities:
@@ -26,6 +26,7 @@ app.use(express.static(root + '/client'));
 //   })
 // );
 
+//Api que retorna en wikidata según texto
 app.get('/api/entitieswiki/:id', async (req, res) => {
   
     try {
@@ -86,7 +87,7 @@ app.get('/api/entitieswiki/:id', async (req, res) => {
 });
 
 
-//Apis CRUD
+//Apis CRUD al Mongo
 //POST
 app.post('/api/entities',jsonParser, async function (req, res, next) {
 
@@ -158,19 +159,19 @@ app.delete('/api/entities/:id', async (req, res) => {
 });
 
 //GET 
-app.get('/api/entities/:id', async (req, res) => {
+app.get('/api/entities/:desc', async (req, res) => {
   try{
     var url =urlMongo;
     const db = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
     const dbo = db.db("saw_21_2");
     var filter = {};
 
-    if(req.params.id != 0)
-      filter = {entity: req.params.id }
+    if(req.params.desc != 0)
+      filter = {description: req.params.desc }
 
     var entities = await dbo.collection('entities').find(filter).toArray();
-    res.send(200,{ entities})
-
+    
+    res.status(200).send(entities)
   }catch(error){
     res.send(500,{ response: 'Ha ocurrido un error: ' + error})
   }
--- client/code.js
+++ client/code.js
@@ -1,36 +1,30 @@
 
 
-async function getEntities() {
-    const response = await fetch('/api/entitieswiki');
-    const data = await response.json();
-    return data
-}
+// async function getEntities() {
+//     const response = await fetch('/api/entitieswiki');
+//     const data = await response.json();
+//     return data
+// }
 
-function fillEntities() {
-    getEntities().then(data => {
-        console.log(data.entities);
-        const ulEntities = document.getElementById("entities");
-        data.entities.forEach(entity => {
-          const liEntity = document.createElement("li");
-          const text = document.createTextNode(entity);
-          liEntity.appendChild(text);
-          ulEntities.appendChild(liEntity);
-        })
-    })
-}
+// function fillEntities() {
+//     getEntities().then(data => {
+//         console.log(data.entities);
+//         const ulEntities = document.getElementById("entities");
+//         data.entities.forEach(entity => {
+//           const liEntity = document.createElement("li");
+//           const text = document.createTextNode(entity);
+//           liEntity.appendChild(text);
+//           ulEntities.appendChild(liEntity);
+//         })
+//     })
+// }
 
-async function getEntitiesFromWikiData() {
-    const response = await fetch('/api/entitieswiki/2500062');
-    console.log("1. :" + JSON.stringify(response.response));
-    const data = await response.json();
-    return data
-}
 
 async function searchEntities() {
     try{
         var valuetoSearch =  document.getElementById("txtEntity").value;
 
-        var obj  = await fetch('/api/entitieswiki/'+valuetoSearch);//await getEntitiesFromWikiData();
+        var obj  = await fetch('/api/entitieswiki/'+valuetoSearch);
         console.log("Objetos obtendi2: ");
         var objtoshow = await obj.json();
         console.log(objtoshow);
@@ -42,26 +36,7 @@ async function searchEntities() {
             liEntity.appendChild(text);
             ulEntities.append(liEntity);
         });
-        
-        // await  getEntitiesFromWikiData().then(async data => {
-            
-        //     console.log('34');
-        //     console.log(data);
-        //     // console.log(JSON.stringify(data));
-         
-
-        // //     const ulEntities = document.getElementById("entities");
-        // //     //data.entities.forEach(entity => {
-        // //         const liEntity = document.createElement("li");
-        // //         console.log(1)
-        // //         const text = document.createTextNode(data);
-        // //         console.log(2)
-        // //         liEntity.appendChild(text);
-        // //         console.log(3)
-        // //         ulEntities.appendChild(liEntity);
-
-        // // // })
-        // })
+     
     }catch(error){
         console.log("Ha ocurrido un error: " +  error);
     }
@@ -81,47 +56,23 @@ async function saveEntity(){
     console.log("Respuesta:", body)
 }
 
+async function searchOwnEntities(){
+    try{
+        var valuetoSearch =  document.getElementById("txtEntity").value;
 
+        var obj  = await fetch('/api/entities/'+valuetoSearch);
+        var objtoshow = await obj.json();
 
-    
-// async function searchEntities(){
-
-//     try {
-//         console.log("1");
-//         var config = {
-//             method: 'get',
-//             url: 'https://en.wikipedia.org/w/rest.php/v1/search/page?q=saturn&limit=10',
-//             headers: {}
-//         };
-       
-//         return  await axios(config)
-//             .then(async function (response) {
-//                 console.log(JSON.stringify(response));
-//                 return response;
-//             })
-//             .catch(function (error) {
-//                 console.log(error);
-//             });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-// //searchEntities();
-//  function searchEntities(){
-
-//       getEntitiesFromWikiData().then(  data => {
-//         console.log(data.data.pages);
-//         const ulEntities = document.getElementById("entities");
-//         data.data.pages.forEach(entity => {
-//           const liEntity = document.createElement("li");
-//           //const text = document.createTextNode(entity);
-//           liEntity.appendChild(entity.description);
-//           ulEntities.appendChild(entity.key);
-//           console.log(entity.description);
-//           console.log(entity.excerpt);
-//           console.log(entity.key);
-//         })
-//     })
-
-
-// }
+        const ulEntities = document.getElementById("entities");
+        document.getElementById("entities").innerHTML = "";
+        objtoshow.forEach(object => {
+            const liEntity = document.createElement("li");
+            const text = document.createTextNode(object.description);
+            liEntity.appendChild(text);
+            ulEntities.append(liEntity);
+        });
+     
+    }catch(error){
+        console.log("Ha ocurrido un error: " +  error);
+    }
+}
